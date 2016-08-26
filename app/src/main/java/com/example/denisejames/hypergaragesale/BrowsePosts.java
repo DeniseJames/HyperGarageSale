d57c15eb46b4ccdb8b34a0db201ca77531a35c39
Denise R James <denisetoo@gmail.com>
2016-08-25 17:51:27 -0700

    Initial commit

---
+++ app/src/main/java/com/example/denisejames/hypergaragesale/BrowsePosts.java
@@ -0,0 +1,14 @@
+package com.example.denisejames.hypergaragesale;
+
+/**
+ * Created by Denise James on 8/25/2016.
+ */
+public class BrowsePosts {
+        public String mTitle;
+        public String mPrice;
+
+        public BrowsePosts (String title, String price) {
+            this.mTitle = title;
+            this.mPrice = price;
+        }
+}
